93469333bb6601226ed0fd6ca8ab2f6900f30a6f
root <root@Jal.(none)>
2016-04-06 15:44:08 +0800

    add send_sms.py get_TXT.py

---
+++ python/get_TXT1.py
@@ -0,0 +1,66 @@
+#coding=utf8
+import urllib2
+import re
+import chardet
+import time
+def spider(url):
+    user_agent ='Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
+    headers = { 'User-Agent' : user_agent }
+    Request = urllib2.Request(url, headers = headers) 
+    Response = urllib2.urlopen(Request)
+    html = Response.read()
+    charset = chardet.detect(html)['encoding']
+    if charset == 'utf-8' or charset == 'UTF-8' or charset == 'utf8':
+        html = html
+    else:
+        html = html.decode('gb2312','ignore').encode('utf-8')
+    unicodehtml = html.decode("utf-8")
+    #获取下一张链接
+    try:
+        Next = re.search('\xe4\xb8\x8a\xe4\xb8\x80\xe7\xab\xa0(.*?)\xe4\xb8\x8b\xe4\xb8\x80\xe7\xab\xa0</a>',html,re.S)
+        Next = Next.group(0)
+        Next = re.search('<a style=\"(.*?)\" href=\"(.*?)\">',Next,re.S)
+        url = "http://%s/%s/%s/%s/%s" %(url.split("/")[2],url.split("/")[3],url.split("/")[4],url.split("/")[5],Next.group(2))
+    except:
+        print "The End"
+    #抓取标题
+#    print unicodehtml
+    try:
+        title = re.search('<H1><a href=(.*?)</a>(.*?)</H1>',unicodehtml,re.S)
+        title = title.group(2)
+        title = title.strip(r"[!@#$%^&*()?;]")
+        title = title.replace(" ","_")
+    except:
+        print "未能捕捉标题"
+        return
+#    抓取正文
+    try:
+        content = re.search('</table>(.*?)<!--',unicodehtml,re.S)
+        content = content.group(1)
+        content = re.sub(r"<br[ ][/]>","\n",content)
+        content = re.sub(r"<(.*)>","",content)
+        content = re.sub(r"&nbsp;"," ",content)
+        content = content.strip()
+        content = re.sub(r"\n"," ",content)
+    except:
+        print "未能捕捉正文"
+        return
+    
+
+    text = {'title':title,'content':content,'Next_url':url}
+    return text
+    
+#
+url = "http://www.piaotian.net/html/3/3946/3868187.html"
+while 1:
+    TXT = spider(url)
+    filename = "D:\python_get_TXT/邪御天娇_%s.txt" %(TXT['title'])
+    f = open(filename,'w+')
+    f.write(TXT['title'])
+    f.write(TXT['content'])
+    f.close()    
+    url = TXT['Next_url']
+    time.sleep(1)
+
+    
+    
--- python/send_sms.py
+++ python/send_sms.py
@@ -3,15 +3,35 @@
 import sys
 import pycurl
 import urllib
- 
+import StringIO
+if len(sys.argv) <3:
+    sys.exit("账号为空或消息为空")
 phone_NO = sys.argv[1]
 message = sys.argv[2]
-if phone_NO == "" or message == "":
-    sys.exit("账号为空或消息为空")
+#phone_NO = "13660666049"
+#message = "Just Test"
 
+url = 'http://222.73.117.158/msg/HttpBatchSendSM?'
 phone_list = phone_NO.split(",")
-
+post_data = {}
 if phone_list != "":
-    for i in phone_list:
-        print i
-
+    for per_NO in phone_list:
+        data = "[验证码]:%s" %message
+        post_data['account'] = "quxuan"
+        post_data['pswd'] =  "NVFA7wACv9QdzX5N"
+        post_data['mobile'] = per_NO
+        post_data['msg'] = data
+        crl = pycurl.Curl()
+        crl.setopt(pycurl.VERBOSE,1)
+        crl.setopt(pycurl.FOLLOWLOCATION, 1)
+        crl.setopt(pycurl.MAXREDIRS, 5)
+        
+        crl.setopt(pycurl.CONNECTTIMEOUT, 30)
+        crl.setopt(pycurl.TIMEOUT, 60)
+        crl.setopt(pycurl.HTTPPROXYTUNNEL,1)
+        crl.fp = StringIO.StringIO()
+        crl.setopt(pycurl.USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)")
+        crl.setopt(crl.POSTFIELDS,urllib.urlencode(post_data))
+        crl.setopt(crl.WRITEFUNCTION, crl.fp.write)
+        crl.setopt(pycurl.URL, url)
+        crl.perform()
